ea4b14cca79b0081672ec2972e5fb27f1e57624f
Mallika hajra <hajramallika999@gmail.com>
2024-11-23 19:36:05 +0530

    Update snake_and_ladder.js

--- snake_and_ladder.js
+++ snake_and_ladder.js
@@ -1,5 +1,106 @@
 const size = 50;
+const numberofBox = 10;
 const winPosition = 100;
+
+function drawBorder(leftSymbol, string, rightSymbol) {
+  return leftSymbol + string + rightSymbol;
+}
+
+function repeat(string, times) {
+  if (times === 0) {
+    return "";
+  }
+
+  return string + repeat(string, times - 1);
+}
+
+function drawTheFooter() {
+  let footer = "";
+
+  for (let index = 1; index < numberofBox; index++) {
+    footer += repeat("━", 4) + '┻';
+  }
+  footer += repeat("━", 4);
+
+  return drawBorder("┗", footer, "┛");
+}
+
+function drawTheHeader() {
+  let header = "";
+
+  for (let index = 1; index < numberofBox; index++) {
+    header += repeat("━", 4) + '┳';
+
+  }
+  header += repeat("━", 4);
+
+  return drawBorder("┏", header, "┓");
+}
+
+function drawLine() {
+  let line = '';
+
+  for (let index = 1; index < numberofBox; index++) {
+    line += repeat("━", 4) + '╋';
+  }
+  line += repeat("━", 4);
+
+  return drawBorder('┣', line, '┫');
+}
+
+function DrawMiddle(number, p1Psition, p2Position) {
+  let string = "";
+  for (let times = 0; times < 10; times++) {
+    if (number == p1Psition || number == p2Position) {
+      string += number == p1Psition ? " 🔴 ┃" : " 🟢 ┃"
+      number = number + 1;
+      continue;
+    }
+
+    if (number % 15 === 0 || number % 17 === 0) {
+      string += number % 15 === 0 ? "🐉  ┃" : "🪜  ┃";
+      number = number + 1;
+      continue;
+    }
+    if (number === winPosition) {
+      string += " 🏆 ┃";
+      number = number + 1;
+      continue;
+    }
+
+    if (number <= 100) {
+      string += number >= 10 ? number + "  ┃" : number + "   ┃";
+
+    }
+    number = number + 1;
+  }
+
+  return "┃" + string;
+}
+
+function drawMiddleFild(p1Psition, p2Position) {
+  let string = "";
+  let boardNumber = 1;
+  let times = 1;
+
+  while (times < 10) {
+    string += DrawMiddle(boardNumber, p1Psition, p2Position) +
+      '\n' + drawLine() + '\n';
+    times++;
+    boardNumber += 10;
+  }
+  string += DrawMiddle(boardNumber, p1Psition, p2Position);
+
+  return string;
+}
+
+function add3String(string1, string2, string3) {
+  return string1 + '\n' + string2 + '\n' + string3;
+}
+
+function DrawTheBox(p1Psition, p2Position) {
+  console.log(add3String(drawTheHeader(), drawMiddleFild(p1Psition, p2Position), drawTheFooter()));
+}
 
 function isThereASnake(position) {
   let priviousPosition = position;
@@ -69,6 +170,7 @@ function rollDice() {
 
 function calculateScore(position) {
   if (confirm(" roll the dice 🎲")) {
+    console.clear();
     const dice = rollDice();
     let currentPosition = position + dice;
 
@@ -82,6 +184,7 @@ function calculateScore(position) {
 
     return currentPosition;
   }
+
   return position;
 }
 
@@ -90,7 +193,6 @@ function startPlay(firstPersonsPosition, secondPersonPosition, p1, p2) {
   console.log(p1, "👩🏽 your privious position is ---", firstPersonsPosition);
 
   firstPersonsPosition = calculateScore(firstPersonsPosition);
-  console.clear();
   DrawTheBox(firstPersonsPosition, secondPersonPosition);
 
   console.log(p1, "👩🏽your next position  is ----", firstPersonsPosition);
@@ -104,7 +206,6 @@ function startPlay(firstPersonsPosition, secondPersonPosition, p1, p2) {
   console.log(p2, "👨🏻 your privious position is ----", secondPersonPosition);
 
   secondPersonPosition = calculateScore(secondPersonPosition);
-  console.clear();
   DrawTheBox(firstPersonsPosition, secondPersonPosition);
 
   console.log(p2, "👨🏻 your next position  is----", secondPersonPosition);
@@ -115,19 +216,21 @@ function startPlay(firstPersonsPosition, secondPersonPosition, p1, p2) {
 
   console.log("___________________________________");
 
+
   return startPlay(firstPersonsPosition, secondPersonPosition, p1, p2);
 }
 
-function play() {
+function getPlayersName() {
   if (confirm("start")) {
     const p1 = prompt("enter the First player name:");
     const p2 = prompt("enter the Second player name:");
-    console.log('\n', p1, 'your token is 🔴\n', p2, 'your token is 🟢\n');
+    console.clear();
+    console.log('\n', p1, ' your token is 🔴\n', p2, ' your token is 🟢\n');
 
     console.log(startPlay(0, 0, p1, p2));
 
+
     if (confirm("do you want to play again 🔁 ")) {
-      console.clear();
       return welcome();
     }
 
@@ -137,109 +240,11 @@ function play() {
   return 0;
 }
 
-function drawTheField(number, p1Score, p2Score) {
-  let string = "";
-
-  for (let times = 0; times < 10; times++) {
-    if (number == p1Score) {
-      string += " 🔴 ┃";
-      number = number + 1;
-      continue;
-    }
-
-    if (number == p2Score) {
-      string += " 🟢 ┃";
-      number = number + 1;
-      continue;
-    }
-
-    if (number === 100) {
-      string += " 🏆 ┃";
-    }
-
-    if (number % 15 === 0) {
-      string += "🐉  ┃";
-    }
-
-    if (number % 17 === 0) {
-      string += "🪜  ┃";
-    }
-
-    if (number < 100 && !(number % 15 === 0) && !(number % 17 === 0)) {
-      string += number >= 10 ? number + "  ┃" : number + "   ┃";
-    }
-
-    number = number + 1;
-  }
-
-  return "┃" + string;
-}
-
-function drawTheFooter(symbol, size) {
-  let footer = "┗";
-
-  for (let index = 1; index < size; index++) {
-    if (index % 5 === 0) {
-      footer += '┻';
-      continue;
-    }
-    footer = footer + symbol;
-  }
-
-  return footer += "┛";
-}
-
-
-function drawTheHeader(symbol, size) {
-  let header = "┏";
-
-  for (let index = 1; index < size; index++) {
-    if (index % 5 === 0) {
-      header += '┳';
-      continue;
-    }
-    header = header + symbol;
-  }
-
-  return header += "┓";
-}
-
-function divideTheField(newString, symbol) {
-  for (let index = 1; index < size; index++) {
-    if (index % 5 === 0) {
-      newString += '╋';
-      continue;
-    }
-    newString += symbol;
-  }
-
-  return newString + '┫';
-}
-
-function drawTheLine(times, field, p1Score, p2Score, number) {
-  if (times === 9) {
-    field += drawTheField(number, p1Score, p2Score);
-    return field;
-  }
-
-  field += drawTheField(number, p1Score, p2Score);
-  field += '\n' + divideTheField("┣", "━") + '\n';
-  number = number + 10;
-
-  return drawTheLine(times + 1, field, p1Score, p2Score, number);
-}
-
-function DrawTheBox(p1Score, p2Score) {
-  console.log(drawTheHeader("━", size));
-  console.log(drawTheLine(0, '', p1Score, p2Score, 1));
-  console.log(drawTheFooter("━", size));
-}
-
 function welcome() {
   console.log("🙏 welcome to  SNAKE and LADDER Game.....\n__________________________________________________\n");
 
   DrawTheBox(0, 0);
-  play();
+  getPlayersName();
 }
 
 welcome();
